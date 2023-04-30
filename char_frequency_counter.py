516eab151c11a114482129193fb49ba56461e4ed
epha-b1 <ephremyonatan27@gmail.com>
2023-04-30 20:36:28 +0300

    some modification is added

--- char_frequency_counter.py
+++ char_frequency_counter.py
@@ -1,10 +1,10 @@
-def char_frequency_counter(string):
+def char_frequency_counter(input_string):
 
     # Initialize an empty dictionary to store the frequency count of each character
     frequency = {}
 
     # Iterate through each character in the string
-    for char in string:
+    for char in input_string:
         # If the character is already in the dictionary, increment its count by 1
         if char in frequency:
             frequency[char] += 1
